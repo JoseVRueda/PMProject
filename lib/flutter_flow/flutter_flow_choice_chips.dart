6a260b2a61d3ff7fe5774720ba91fa1e3fdca109
Yuli Andrea Meza Barros <83044932+yulimezabarros@users.noreply.github.com>
2024-04-22 08:26:52 -0500

    Update

--- lib/flutter_flow/flutter_flow_choice_chips.dart
+++ lib/flutter_flow/flutter_flow_choice_chips.dart
@@ -1,5 +1,4 @@
 import 'form_field_controller.dart';
-
 import 'package:flutter/material.dart';
 import 'package:flutter/scheduler.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
